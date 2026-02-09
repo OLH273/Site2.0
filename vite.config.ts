3312989354c5a51f93e2fde5f15e549a0c218720
OLH273 <oli_1_harrison@hotmail.com>
2026-02-09 13:15:36 +0000

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Site2.0/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
